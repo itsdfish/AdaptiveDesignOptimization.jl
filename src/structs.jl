5c0899b71473a86e053f68111d7a4164ee5d392c
itsdfish <itsdfish@gmail.com>
2021-02-22 04:42:38 -0500

    updates to functions

--- src/structs.jl
+++ src/structs.jl
@@ -1,15 +1,12 @@
-using Distributions, Parameters
+using Distributions, Parameters, StatsFuns
 import Base.Iterators: product
-
-abstract type AbstractTask end
-
 mutable struct Model{F<:Function,P}
     prior::P
     loglike::F
 end
 
-function Model(args...; prior, loglike, kwargs...)
-    return Model(prior, x->loglike(x..., args...; kwargs...))
+function Model(args...; prior=nothing, loglike, kwargs...)
+    return Model(prior, (x...)->loglike(x..., args...; kwargs...))
 end
 
 """
@@ -17,7 +14,6 @@ end
 
 `Optimizer` constructs a model object for adaptive design optimization
 
-- `task`: utility curvature for gains
 - `model`: utility curvature for losses
 - `grid_design`: probability weighting parameter for gains 
 - `grid_parms`: probability weighting parameter for losses
@@ -28,67 +24,118 @@ Constructor
 Optimizer(;task, model, grid_design, grid_parms, grid_response)
 ````
 """
-mutable struct Optimizer{T,M<:Model,T1,T2,T3,T4}
-    task::T
+mutable struct Optimizer{M<:Model,T1,T2,T3,T4,T5,T6}
     model::M
     design_grid::T1
     parm_grid::T2
     data_grid::T3
-    likelihoods::T4
+    log_like::Array{Float64,3}
+    marg_log_like::T4
+    priors::T5
+    log_post::Vector{Float64}
+    entropy::T6
+    marginal_entropy::Vector{Float64}
+    conditional_entropy::Vector{Float64}
 end
 
-function Optimizer(;task=nothing, model, design_grid, parm_grid, data_grid)
-    dims = [length.(values(data_grid))...; 
-        length.(values(parm_grid))...;  
-        length.(values(design_grid))...]
-    likelihoods = fill(0.0, dims...)
-    return Optimizer(task, model, design_grid, parm_grid, data_grid, likelihoods)
+function Optimizer(;model, design_grid, parm_grid, data_grid)
+    log_like = compute_log_like(model, design_grid, parm_grid, data_grid)
+    priors = compute_prior(model, parm_grid)
+    log_post = log.(priors[:])
+    entropy = compute_entropy(log_like)
+    marginal_entropy = nothing
+    conditional_entropy = nothing
+    return Optimizer(model, design_grid, parm_grid, data_grid, log_like,
+        priors, entropy, marginal_entropy, conditional_entropy)
 end
 
-function compute_likelihoods!(optimizer)
-    @unpack model, design_grid, parm_grid, data_grid = optimizer
-    grid = product(data_grid..., parm_grid..., design_grid...)
-    optimizer.likelihoods .= map(x->model.loglike(x) |> exp, grid)
+function compute_prior(model::Model, parm_grid)
+    return compute_prior(model.prior, parm_grid)
 end
 
-function compute_marg_entropy()
+function compute_prior(prior, parm_grid)
+    dens = [mapreduce((θ,d)->pdf(d, θ), *, g, prior) for g in parm_grid]
+    return dens/sum(dens)
+end
+
+function compute_prior(prior::Nothing, parm_grid)
+    return fill(1/length(parm_grid), size(parm_grid))
+end
+
+function compute_log_like(model::Model, design_grid, parm_grid, data_grid)
+    return compute_log_like(model.loglike, design_grid, parm_grid, data_grid)
+end
+
+function compute_log_like(loglike, design_grid, parm_grid, data_grid)
+    LLs = zeros(length(parm_grid), length(design_grid), length(data_grid))
+    for (d, datum) in enumerate(data_grid)
+        for (k,design) in enumerate(design_grid)
+            for (p,parm) in enumerate(parm_grid)
+                LLs[p,k,d] = loglike(parm..., design..., datum...) 
+            end
+        end
+    end
+    return LLs
+end
+
+function compute_marg_log_like(optimizer)
+    @unpack log_like, log_post = optimizer
+    return compute_marg_log_like(log_post, log_like)
+end
+
+function compute_marg_log_like(log_post, log_like)
+    return logsumexp(log_post .+ log_like, dims=1)
+end
+
+function compute_marg_log_like!(optimizer)
+    optimizer.marg_like .= compute_marg_log_like(optimizer) 
+    return nothing
+end
+
+function compute_marg_post(optimizer)
+    @unpack posteriors = optimizer
+    return map(d->sum(posterior, dims=d), ndims(posterior):-1:1)
+end
+
+function compute_cond_entropy(entropy, post)
+    return entropy'*post
+end
+
+function compute_entropy(log_like)
+    return -1*sum(exp.(log_like) .* log_like, dims=3)[:,:]
+end
+
+function compute_marg_entropy!(optimizer::Optimizer)
+    @unpack marg_log_like = optimizer
+    return compute_marg_entropy!(marg_log_like)
+end
+
+function compute_marg_entropy!(marg_log_like)
+    return -sum(exp.(marg_log_like).*marg_log_like, dims=3)[:]
+end
+
+function update!()
+    #update posterior
+    #
 
 end
 
-function compute_cond_entropy()
-
+function update_posterior!(optimizer, data)
+    @unpack log_post = optimizer
+    da = findfirst(x->x == data, data_grid)
+    dn = find
+    log_post .+= log_like[:,dn,da]
+    log_post .-= logsumexp(log_post)
+    return nothing 
 end
 
-
-
-
-discount(t, κ) = 1/(1 + κ*t)
-
-function loglike(data, κ, τ, t_ss, t_ll, r_ss, r_ll)
-    u_ll = r_ll * discount(t_ll, κ)
-    u_ss = r_ss * discount(t_ss, κ)
-    p = 1/(1 + exp(-τ * (u_ll - u_ss)))
-    p = max(p, 1e-10)
-    LL = data ? log(p) : log(1 - p)
-    return LL
+function find_index(grid, val)
+    i = 1
+    for g in grid 
+        if g == val 
+            return i
+        end
+        i += 1
+    end 
+    return i
 end
-
-prior = [Exponential(.1), Uniform(0, 5)]
-
-model = Model(;prior, loglike)
-
-parm_grid = (κ = range(-5, 0, length=50) .|> x->10^x, 
-   τ = range(0, 5, length=11)[2:end])
-
-   design_grid = (
-    t_ss = [0.0], 
-    t_ll = [0.43, 0.714, 1, 2, 3,
-        4.3, 6.44, 8.6, 10.8, 12.9,
-        17.2, 21.5, 26, 52, 104,
-        156, 260, 520], 
-    r_ss = 12.5:12.5:800,
-    r_ll = [800.0])
-
-data_grid = (choice=[true, false],)
-
-optimizer = Optimizer(;design_grid, parm_grid, data_grid, model)
